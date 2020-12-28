68bcb64c4b10f4aae508297a78af96e5a1d5ed02
lcgamboa <lcgamboa@yahoo.com>
2020-12-28 13:32:19 -0300

    examples updated

--- examples/unpacked/help.sh
+++ examples/unpacked/help.sh
@@ -23,24 +23,33 @@ do
   cd $board	
   echo "board: $board"	
   mkdir -p "../../help/$board/"
-  echo "<hr><br><h1><a name="$board"></a>$board</h1>" >> ../../help/examples_index.html 
-  echo "<table style=\"width:98%\" border=\"1\">" >> ../../help/examples_index.html 
-  echo "<tr><th width=\"20%\">Processor</th><th>Description</th></tr>" >> ../../help/examples_index.html 
+  echo "<hr><a name="$board"></a> <h1><br>Examples: $board</h1>" >> ../../help/examples_index.html 
+  for proc in `find * -maxdepth 0 -type d,l 2> /dev/null`
+  do
+    cd $proc	
+    for name in `find * -maxdepth 0 -type d 2> /dev/null`
+    do
+      echo "<br><a href=\"#${board}_${proc}_${name}\">$proc/$name</a>" >> ../../../help/examples_index.html 
+    done	    
+    cd ..
+  done	  
   for proc in `find * -maxdepth 0 -type d,l 2> /dev/null`
   do
     cd $proc	
     echo "board: $board  proc: $proc"	
-    echo -n "<tr><td><b>$proc <a name=\"$board" >> ../../../help/examples_index.html 
+    echo -n "<a name=\"$board" >> ../../../help/examples_index.html 
     echo -n "_" >> ../../../help/examples_index.html 
-    echo "$proc\"></a></b></td><td><table style=\"width:100%\" border=\"1\">" >>  ../../../help/examples_index.html 
+    echo "$proc\"></a>" >>  ../../../help/examples_index.html 
     for name in `find * -maxdepth 0 -type d 2> /dev/null`
     do
       echo "board: $board  proc: $proc  directory: $name"	
       mkdir -p "../../../help/$board/$proc/$name/"
+      html=`sed -n '/<body/,/<\/body>/{//!p}' $name/Readme.html`
       cp -R "$name/Readme.html" "../../../help/$board/$proc/$name/" 
       cp -R "$name/src/" "../../../help/$board/$proc/$name/"
       cp -R "$name/$name.png" "../../../help/$board/$proc/$name/"
-      echo "<tr><td><a name=\"${board}_${proc}_${name}\"></a>$name</td><td width=\"15%\"><a target=\"blank_\" href=\"$board/$proc/$name/$name.png\"><img src='$board/$proc/$name/$name.png' width=200></a></td><td width=\"20%\"><a href=$board/$proc/$name/Readme.html>Help / Source Code</a>" >> ../../../help/examples_index.html 
+      echo "<hr><table style=\"width:100%\" border=\"0\">" >>  ../../../help/examples_index.html
+      echo "<tr><td colspan=4><a name=\"${board}_${proc}_${name}\"></a><div style=\"color:gray;\"><small>[$board/$proc/$name]</small></div>${html}<br><br></td></tr><tr><td width=\"15%\"><a target=\"blank_\" href=\"$board/$proc/$name/$name.png\"><img src='$board/$proc/$name/$name.png' width=200></a></td>" >> ../../../help/examples_index.html 
       echo "<td width=\"20%\"><a href=\"../pzw/$board/$proc/$name.pzw\" target=\"_blank\" >Download (pzw)</a></td>" >> ../../../help/examples_index.html 
       echo "<td width=\"20%\">" >> ../../../help/examples_index.html
       if [ ! -f "${name}/no_online" ]; then
@@ -48,12 +57,10 @@ do
       echo "<a href=\"../../js/picsimlab_mt.html?../picsimlab_examples/pzw/$board/$proc/$name.pzw\" target=\"_blank\" >Online (wasm mt)</a><br>" >> ../../../help/examples_index.html 
       echo "<a href=\"../../js/picsimlab_asmjs.html?../picsimlab_examples/pzw/$board/$proc/$name.pzw\" target=\"_blank\" >Online (asm.js)</a>" >> ../../../help/examples_index.html
       fi 
-      echo "</td></tr>" >> ../../../help/examples_index.html 
+      echo "</td></tr></table>" >> ../../../help/examples_index.html 
     done
-    echo "</table></td></tr>" >> ../../../help/examples_index.html
     cd ..
   done
-  echo "</table>" >> ../../help/examples_index.html
   cd ..
 done
 ./parts.sh >> ../help/examples_index.html
--- examples/unpacked_exp/help.sh
+++ examples/unpacked_exp/help.sh
@@ -13,37 +13,44 @@ do
   cd $board	
   echo "board: $board"	
   mkdir -p "../../help/$board/"
-  echo "<hr><br><h1><a name="$board"></a>$board</h1>" >> ../exp.html 
-  echo "<table style=\"width:98%\" border=\"1\">" >> ../exp.html 
-  echo "<tr><th width=\"20%\">Processor</th><th>Description</th></tr>" >> ../exp.html 
+  echo "<hr><a name="$board"></a> <h1><br>Examples: $board</h1>" >> ../exp.html 
+  for proc in `find * -maxdepth 0 -type d,l 2> /dev/null`
+  do
+    cd $proc	
+    for name in `find * -maxdepth 0 -type d 2> /dev/null`
+    do
+      echo "<br><a href=\"#${board}_${proc}_${name}\">$proc/$name</a>" >> ../../exp.html 
+    done	    
+    cd ..
+  done	  
   for proc in `find * -maxdepth 0 -type d,l 2> /dev/null`
   do
     cd $proc	
     echo "board: $board  proc: $proc"	
-    echo -n "<tr><td><b>$proc <a name=\"$board" >> ../../exp.html 
+    echo -n "<a name=\"$board" >> ../../exp.html 
     echo -n "_" >> ../../exp.html 
-    echo "$proc\"></a></b></td><td><table style=\"width:100%\" border=\"1\">" >>  ../../exp.html 
+    echo "$proc\"></a>" >>  ../../exp.html 
     for name in `find * -maxdepth 0 -type d 2> /dev/null`
     do
       echo "board: $board  proc: $proc  directory: $name"	
       mkdir -p "../../../help/$board/$proc/$name/"
+      html=`sed -n '/<body/,/<\/body>/{//!p}' $name/Readme.html`
       cp -R "$name/Readme.html" "../../../help/$board/$proc/$name/" 
       cp -R "$name/src/" "../../../help/$board/$proc/$name/"
       cp -R "$name/$name.png" "../../../help/$board/$proc/$name/"
-      echo "<tr><td><a name=\"${board}_${proc}_${name}\"></a>$name</td><td width=\"15%\"><a target=\"blank_\" href=\"$board/$proc/$name/$name.png\"><img src='$board/$proc/$name/$name.png' width=200></a></td><td width=\"20%\"><a href=$board/$proc/$name/Readme.html>Help / Source Code</a>" >> ../../exp.html 
-      echo "<td width=\"20%\"><a href=\"../pzw_exp/$board/$proc/$name.pzw\" target=\"_blank\" >Download (pzw)</a></td>" >> ../../exp.html 
+      echo "<hr><table style=\"width:100%\" border=\"0\">" >>  ../../exp.html
+      echo "<tr><td colspan=4><a name=\"${board}_${proc}_${name}\"></a><div style=\"color:gray;\"><small>[$board/$proc/$name]</small></div>${html}<br><br></td></tr><tr><td width=\"15%\"><a target=\"blank_\" href=\"$board/$proc/$name/$name.png\"><img src='$board/$proc/$name/$name.png' width=200></a></td>" >> ../../exp.html 
+      echo "<td width=\"20%\"><a href=\"../pzw/$board/$proc/$name.pzw\" target=\"_blank\" >Download (pzw)</a></td>" >> ../../exp.html 
       echo "<td width=\"20%\">" >> ../../exp.html
       if [ ! -f "${name}/no_online" ]; then
          echo "<a href=\"../../js_exp/picsimlab.html?../picsimlab_examples/pzw_exp/$board/$proc/$name.pzw\" target=\"_blank\" >Online (wasm)</a><br>" >> ../../exp.html 
          echo "<a href=\"../../js_exp/picsimlab_mt.html?../picsimlab_examples/pzw_exp/$board/$proc/$name.pzw\" target=\"_blank\" >Online (wasm mt)</a><br>" >> ../../exp.html 
          echo "<a href=\"../../js_exp/picsimlab_asmjs.html?../picsimlab_examples/pzw_exp/$board/$proc/$name.pzw\" target=\"_blank\" >Online (asm.js)</a>" >> ../../exp.html
       fi
-      echo "</td></tr>" >> ../../exp.html 
+      echo "</td></tr></table>" >> ../../exp.html 
     done
-    echo "</table></td></tr>" >> ../../exp.html
     cd ..
   done
-  echo "</table>" >> ../exp.html
   cd ..
 done
 ./parts.sh >> exp.html
