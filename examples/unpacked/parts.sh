675d367bed4515b31bfbb6fb66a3ba08db101814
lcgamboa <lcgamboa@yahoo.com>
2021-01-24 22:05:19 -0300

    new parts added

--- examples/unpacked/parts.sh
+++ examples/unpacked/parts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
-declare -a parts=("7 Segments Display" "Buzzer" "D. Transfer function" "ETH w5500" "Gamepad" "Gamepad (Analogic)" \
-	"IO 74xx595" "IO MCP23S17" \
+declare -a parts=("7 Segments Display" "Buzzer" "D. Transfer function" "DC Motor" "ETH w5500" "Encoder" "Gamepad" \
+	"Gamepad (Analogic)" "IO 74xx595" "IO MCP23S17" \
         "IO PCF8574" "IO UART" "IO Virtual term" "Jumper Wires" "Keypad" "LCD hd44780"  "LCD ili9341" "LCD pcf8833"\
       	"LCD pcd8544"  \
-	"LCD ssd1306" "LED Matrix" "LEDs"  "MEM 24CXXX" "Potentiometers" \
+	"LCD ssd1306" "LED Matrix" "LEDs"  "MEM 24CXXX" "Potentiometers" "Potentiometers (Rotary)" \
         "Push buttons"  "Push buttons (Analogic)"  "RGB LED"  "RTC ds1307"  "RTC pfc8563" "SD Card" "Servo motor"\
 	"Signal Generator" "Step motor" "Switchs" "Temperature System" "VCD Dump" "VCD Dump (Analogic)" "VCD Play" )
 
