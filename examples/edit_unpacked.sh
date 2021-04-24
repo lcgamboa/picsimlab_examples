c48ea79b5708765b493291ebc5a2f6c23d795f55
lcgamboa <lcgamboa@yahoo.com>
2021-04-24 14:24:02 -0300

    chg: examples updated !minor

---
+++ examples/edit_unpacked.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+echo $1
+dir=`pwd`
+rm -rf /tmp/picsimlab_workspace/
+cp -Rv "$1" /tmp/picsimlab_workspace/
+cd /tmp
+rm -f test.pzw
+zip -rX test.pzw picsimlab_workspace -x \*no_online \*.png \*src/\*
+picsimlab test.pzw
+#user must save the workspace as /tmp/test.pzw
+rm -rf picsimlab_workspace/
+unzip test.pzw
+cd picsimlab_workspace/
+cp -Rv * "$dir/$1"
