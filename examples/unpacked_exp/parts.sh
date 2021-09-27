5d3658da1fca2a4d2e51fc040cd74bb4935507f5
lcgamboa <lcgamboa@yahoo.com>
2021-09-27 14:46:11 -0300

    examples updated

--- examples/unpacked_exp/parts.sh
+++ examples/unpacked_exp/parts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 declare -a parts=("7 Segments Display" "7 Segments Display (Decoder)" "Buzzer" "D. Transfer Function" "DC Motor" \
-	"ETH w5500" "Encoder" "FM50 (Temperature)" "Fixed Voltage" "Gamepad" \
+	"DS1621 (Temperature I2C)" "ETH w5500" "Encoder" "FM50 (Temperature)" "Fixed Voltage" "Gamepad" \
 	"Gamepad (Analogic)" "IO 74xx595" "IO MCP23S17" \
 	"IO PCF8574" "IO UART" "IO Virtual Term" "Jumper Wires" "Keypad" "LM35 (Temperature)" "LCD hd44780"  \
 	"LCD ili9341" "LCD pcf8833" "LCD pcd8544"  \
