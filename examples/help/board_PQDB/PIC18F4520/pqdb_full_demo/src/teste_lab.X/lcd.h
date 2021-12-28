f510666f5870222386d775894b92975082a7433c
lcgamboa <lcgamboa@yahoo.com>
2021-12-28 11:23:21 -0300

    examples updated

--- examples/help/board_PQDB/PIC18F4520/pqdb_full_demo/src/teste_lab.X/lcd.h
+++
@@ -1,9 +0,0 @@
-#ifndef LCD
-#define LCD
-  void lcdCommand(char value);
-  void lcdChar(char value);
-  void lcdString(char msg[]);
-  void lcdNumber(int value);
-  void lcdPosition(int line, int col);
-  void lcdInit(void);
-#endif
--- examples/help/board_PQDB/PIC18F4520/pqdb_full_demo/src/teste_lab.X/so.h
+++
@@ -1,7 +0,0 @@
-#ifndef SO_H_
-#define SO_H_
-
-	void soInit (void);
-	void soWrite(int value);
-
-#endif
