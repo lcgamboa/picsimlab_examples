105cdda06d2f112328168c424d5012daf049f5c4
lcgamboa <lcgamboa@yahoo.com>
2021-12-30 19:34:10 -0300

    LDR part added

--- examples/unpacked/partsname.sh
+++ examples/unpacked/partsname.sh
@@ -2,7 +2,7 @@
 declare -a parts=("7 Segments Display" "7 Segments Display (Decoder)" "Buzzer" "D. Transfer Function" "DC Motor" \
 	"DS1621 (Temperature I2C)" "ETH w5500" "Encoder" "FM50 (Temperature)" "Fixed Voltage" "Gamepad" \
 	"Gamepad (Analogic)" "IO 74xx595" "IO MCP23S17" \
-	"IO PCF8574" "IO UART" "IO Virtual Term" "Jumper Wires" "Keypad" "LM35 (Temperature)" "LCD hd44780"  \
+	"IO PCF8574" "IO UART" "IO Virtual Term" "Jumper Wires" "Keypad" "LDR" "LM35 (Temperature)" "LCD hd44780"  \
 	"LCD ili9341" "LCD pcf8833" "LCD pcd8544"  \
 	"LCD ssd1306" "LED Matrix" "LEDs"  "MEM 24CXXX" "MPU6050" "Potentiometers" "Potentiometers (Rotary)" \
         "Push Buttons"  "Push Buttons (Analogic)"  "RGB LED"  "RTC ds1307"  "RTC pfc8563" "SD Card" "Servo Motor"\
